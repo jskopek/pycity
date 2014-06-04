8b3551710ef62d060fa7c8240ad9c0b58ae83daf
Jean-Marc Skopek <jskopek@gmail.com>
2014-06-04 13:23:19 -0700

    Attempting to fix manifest

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+  name = 'pycity',
+  packages = ['pycity'], # this must be the same as the name above
+  version = '0.1',
+  description = 'Provides programmatic access to a list of the largest US Cities',
+  author = 'Jean-Marc Skopek',
+  author_email = 'jskopek@gmail.com',
+  url = 'https://github.com/jskopek/pycity', # use the URL to the github repo
+  download_url = 'https://github.com/jskopek/pycity/archive/0.1.tar.gz',
+  keywords = [],
+  classifiers = [],
+)
