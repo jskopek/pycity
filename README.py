4272d84de92a570b2f3b58ba4e1480abe25eb89c
Jean-Marc Skopek <jskopek@gmail.com>
2014-06-04 13:05:40 -0700

    Added basic README

---
+++ README.py
@@ -0,0 +1,15 @@
+Provides programmatic access to a list of the largest US Cities. Currently contains the top 140 more populus cities in the US
+
+Usage::
+
+    >>> import pycity
+    >>> pycity.cities.get(code='US-AL')
+    [
+            {u'code': u'US-AL', u'name': u'Birmingham', u'population': 212113}, 
+            {u'code': u'US-AL', u'name': u'Montgomery', u'population': 201332}, 
+            {u'code': u'US-AL', u'name': u'Mobile', u'population': 194899}, 
+            {u'code': u'US-AL', u'name': u'Huntsville', u'population': 186254}
+    ]
+
+The `get` method can take one of three keys - 'code', 'name', or 'population' - as a filter
+
