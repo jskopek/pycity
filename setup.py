cb9aede724643e9b5b8bcbf5f208c559a2aaf927
Jean-Marc Skopek <jskopek@gmail.com>
2014-06-04 13:40:09 -0700

    Fixed duplicate property bug

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ from setuptools import find_packages
 setup(
   name = 'pycity',
   packages = ['pycity'], # this must be the same as the name above
+  include_package_data=True,
   version = '0.1.2',
   description = 'Provides programmatic access to a list of the largest US Cities',
   author = 'Jean-Marc Skopek',
@@ -11,7 +12,5 @@ setup(
   url = 'https://github.com/jskopek/pycity', # use the URL to the github repo
   download_url = 'https://github.com/jskopek/pycity/archive/0.1.2.tar.gz',
   keywords = [],
-  packages=find_packages(),
-  include_package_data=True,
   classifiers = [],
 )
